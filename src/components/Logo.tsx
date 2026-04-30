e557f3aead6d2af6f94c0968976c6f71f331b188
Wes <wes@miragecorp.org>
2026-04-30 00:05:48 -0700

    feat: differnet font size

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ export default function Logo() {
     return (
         <a href="/" className="flex items-center gap-1 cursor-pointer">
             <img src="/icon.svg" alt="Honch Logo" className="w-7 h-7" />
-            <span className="text-xl font-bold tracking-tight">Honch</span>
+            <span className="text-2xl font-bold tracking-tight">Honch</span>
         </a>
     );
 }
